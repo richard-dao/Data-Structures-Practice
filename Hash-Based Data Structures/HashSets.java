c1b9da833f92bfe7639770efdc116ffff7616737
Richard Dao <41171031+richard-dao@users.noreply.github.com>
2021-03-19 23:50:13 -0700

    Update HashSets.java

--- Hash-Based Data Structures/HashSets.java
+++ Hash-Based Data Structures/HashSets.java
@@ -1,1 +1,21 @@
-
+Set<String> hashSet = new HashSet<String>();
+		hashSet.add("Zero");
+		hashSet.add("Three");
+		hashSet.add("One");
+		hashSet.add("Two");
+		hashSet.add("Zero");
+		hashSet.add("Negative One");
+		hashSet.add("Negative Two");
+		hashSet.remove("Negative Two");
+		hashSet.remove("Zero");
+	
+		Iterator<String> itr = hashSet.iterator();
+		while (itr.hasNext()) {
+			String next = itr.next();
+			System.out.print(next + ", ");
+			itr.remove();
+		}
+		
+		for (String s : hashSet) {
+			System.out.print(s + ", ");
+		}
