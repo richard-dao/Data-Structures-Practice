6637caa65737f841e2f8f7e1c0977c98ac4faeca
Richard Dao <41171031+richard-dao@users.noreply.github.com>
2021-04-26 16:43:10 -0700

    Create Stacks.cpp

---
+++ Linear Structures/Stacks.cpp
@@ -0,0 +1,104 @@
+//Node.hpp
+struct Node {
+    public:
+        int value;
+        Node* down;
+        Node(int value);
+};
+
+//Node.cpp
+#include <iostream>
+#include "Node.hpp"
+using namespace std;
+Node::Node(int val){
+    value = val;
+    down = NULL;
+}
+
+//Stack.hpp
+#include "Node.cpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stack{
+    public:
+        Node* top;
+        Stack();
+        bool isEmpty();
+        int peek();
+        void push(int value);
+        void pop();
+        std::vector<int> display();
+
+};
+
+//Stack.cpp
+#include "Stack.hpp"
+#include <iostream>
+using namespace std;
+Stack::Stack(){
+    top = NULL;
+}
+
+bool Stack::isEmpty(){
+    return top == NULL;
+}
+
+int Stack::peek(){
+    return top->value;
+}
+
+void Stack::push(int value){
+    if (top == NULL){
+        top = new Node(value);
+        return;
+    }
+    Node* newNode = new Node(value);
+    newNode->down = top;
+    top = newNode;
+}
+
+void Stack::pop(){
+    if (top == NULL){
+        return;
+    }
+    top = top->down;
+}
+
+std::vector<int> Stack::display(){
+    std::vector<int> rArr;
+    Node* current = top;
+    while (current != NULL){
+        rArr.push_back(current->value);
+        current = current->down;
+    }
+    return rArr;
+}
+
+
+//main.cpp
+#include <iostream>
+#include "Stack.cpp"
+using namespace std;
+
+void printList(std::vector<int> l){
+    for(int i = 0; i < l.size(); i++){
+        std::cout << l[i] << " -> ";
+    }
+    std::cout << "\n";
+
+}
+
+int main(){
+	Stack s;
+	std::cout << s.isEmpty() << "\n";
+	s.push(0);
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.pop();
+	printList(s.display());
+	std::cout << "Top: " << s.peek() << "\n";
+	
+}
