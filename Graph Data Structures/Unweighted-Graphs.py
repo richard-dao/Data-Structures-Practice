140538301f11e51359b87a9349b4c60087cd36e5
Richard Dao <41171031+richard-dao@users.noreply.github.com>
2021-11-09 20:09:08 -0800

    Update Unweighted-Graphs.py

--- Graph Data Structures/Unweighted-Graphs.py
+++ Graph Data Structures/Unweighted-Graphs.py
@@ -46,6 +46,47 @@ class DFS(object):
     def hasPathTo(self, t):
         return self.marked[t]
 
+class BFS(object):
+    def __init__(self, graph, s):
+        self.s = s
+        self.marked = [False] * graph.V
+        self.edgeTo = [None] * graph.V
+        self.bfs(graph, s)
+    
+    def bfs(self, graph, v):
+        self.queue = []
+        self.marked[self.s] = True
+        self.queue.append(self.s)
+
+        while (len(self.queue) > 0):
+            n = self.queue.pop(0)
+            adj = graph.returnAdj(n)
+            for i in adj:
+                if not self.marked[i]:
+                    self.queue.append(i)
+                    self.marked[i] = True
+                    self.edgeTo[i] = n
+    
+    def shortestPath(self, t):
+        if (self.hasPathTo(t) == False):
+            return None
+        path = list()
+        i = t
+        while (i != self.s):
+            path.append(i)
+            i = self.edgeTo[i]
+        path.append(self.s)
+        return path[::-1]
+    
+    def hasPathTo(self, t):
+        return self.marked[t]
+    
+    def getMarked(self):
+        return self.marked
+    
+    def getEdgeTo(self):
+        return self.edgeTo
+    
 g = Graph(9)
 g.addEdge(0, 1)
 g.addEdge(1, 2)
@@ -57,8 +98,13 @@ g.addEdge(5, 6)
 g.addEdge(5, 8)
 g.addEdge(6, 7)
 g.print();
-d = DFS(g, 0)
+d = DFS(g, 2)
 p = d.pathTo(7)
 for x in p:
     print(x, end=" ")
-print(d.hasPathTo(9))
+print(d.hasPathTo(8))
+b = BFS(g, 2)
+
+l = b.shortestPath(7)
+for x in l:
+    print(x, end=" ")
